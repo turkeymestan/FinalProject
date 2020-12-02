fb62ddd7445694aceed18f9a57e2a6f95d97633a
LaurenBenfield <74433930+LaurenBenfield@users.noreply.github.com>
2020-12-01 20:03:58 -0600

    Update FinalProject..m

--- FinalProject..m
+++ FinalProject..m
@@ -1,5 +1,13 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Goal of Script
+% Final project We are attempting to code a behavioral experiment 
+% using a posner cueing paradigm. A cue will be presented, pointing 
+% to one side of the visual field. Two images will then be presented, 
+% one of a subject with a gun, the other a subject without a gun. The 
+% test subject must press the 'F' key if the cue points towards the gun, 
+% and must press the 'j' key if the cue does not point towards the gun. 
+% Reaction time as a function of accuracy will be studied, and we will 
+% attempt to determine if the cueing had any effect on the accuracy/reaction time.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Clear all
